c12ed06504b05f8f5f53f911c2b3e8fdd5ac333c
Bhargav-YBS <bhargavsaiyekkala2005@gmail.com>
2026-09-09 23:00:39 +0530

    Add files via upload

---
+++ Day_03.java
@@ -0,0 +1,16 @@
+//  Taking input from user and reversing the string 
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input a string: ");
+        char[] letters = sc.nextLine().toCharArray();
+        System.out.print("Reverse string: ");
+        for (int i = letters.length - 1; i >= 0; i--) {
+            System.out.print(letters[i]);
+        }
+        System.out.print("\n");
+    }
+}
