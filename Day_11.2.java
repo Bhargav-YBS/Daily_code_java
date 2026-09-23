ddf7d8d0b35e2d0f52f1d41e9423fedf073e832f
Bhargav-YBS <bhargavsaiyekkala2005@gmail.com>
2026-09-23 23:45:27 +0530

    Create Day_11.2.java

---
+++ Day_11.2.java
@@ -0,0 +1,107 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeLevelOrderTraversal102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        int level = 0;
+
+        if (root == null) {
+            return result;
+        } else {
+            q.add(root);
+        }
+
+        helper(q, result, 0);
+
+        return result;
+    }
+    private void helper(Queue<TreeNode> q, List<List<Integer>> result, int level) {
+        int s = q.size();
+        if (s == 0) {
+            return;
+        }
+
+        addLevel(result, level);
+
+        int i = 0;
+        while (i < s) {
+            TreeNode now = q.poll();
+            result.get(level).add(now.val);
+            if (now.left != null) {
+                q.add(now.left);
+            }
+            if (now.right != null) {
+                q.add(now.right);
+            }
+            i++;
+        }
+
+        helper(q, result, level + 1);
+    }
+    private void addLevel(List<List<Integer>> result, int level) {
+        while (result.size() <= level) {
+            result.add(new ArrayList<Integer>());
+        }
+    }
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+
+        if(root == null) return wrapList;
+
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<Integer>();
+            for(int i=0; i<levelNum; i++) {
+                if(queue.peek().left != null) queue.offer(queue.peek().left);
+                if(queue.peek().right != null) queue.offer(queue.peek().right);
+                subList.add(queue.poll().val);
+            }
+            wrapList.add(subList);
+        }
+        return wrapList;
+    }
+    public List<List<Integer>> levelOrder3(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int cnt = queue.size();
+            for (int i = 0; i < cnt; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        levelHelper(res, root, 0);
+        return res;
+    }
+
+    public void levelHelper(List<List<Integer>> res, TreeNode root, int height) {
+        if (root == null) return;
+        if (height >= res.size()) {
+            res.add(new LinkedList<Integer>());
+        }
+        res.get(height).add(root.val);
+        levelHelper(res, root.left, height+1);
+        levelHelper(res, root.right, height+1);
+    }
+
+}
